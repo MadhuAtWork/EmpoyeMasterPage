499e8b930bc5149877a8d99143bbc26069339e89
madhu <109514968+MadhuAtWork@users.noreply.github.com>
2024-02-15 19:45:35 +0530

    Add files via upload
    Added Employemaster Page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Masterpage from "./components/Masterpage"
+
+function App() {
+  return (
+    <>
+      <Masterpage></Masterpage>
+    </>
+  );
+}
+
+export default App;
